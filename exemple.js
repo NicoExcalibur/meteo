f92567e5c6704ee00dc3ecff0f23aff46f974c72
NicoEcxalibur <nico.zanz@gmail.com>
2021-04-26 17:56:06 +0200

    add request & display temperature + city

---
+++ exemple.js
@@ -0,0 +1,25 @@
+let ville = "Reims";
+let apiId = '227cf9ef448c3934d2b5f68324038f5d';
+ 
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=' + apiId +'&units=metric';
+
+let request = new XMLHttpRequest(); // créer un objet
+request.open('GET', url); //
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+    if (request.readyState === XMLHttpRequest.DONE ) {
+      if (request.status === 200) {
+        let response = request.response;
+        let temp = response.main.temp;
+        console.log(temp);
+
+        document.querySelector('#temperature_label').textContent = temp;
+        document.querySelector('#ville').textContent = ville;
+      }
+    } else {
+        alert('Un problème est survenu, revenez plus tard.');
+    }
+}
+
