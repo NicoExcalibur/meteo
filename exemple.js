d438674667f47cfce82552da6b7966e4dd2ef8db
NicoEcxalibur <nico.zanz@gmail.com>
2021-04-26 18:24:57 +0200

    add function to get the temperature of the chosen city

--- exemple.js
+++ exemple.js
@@ -1,30 +1,36 @@
-let city = "";
-let apiId = '227cf9ef448c3934d2b5f68324038f5d';
- 
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=' + apiId +'&units=metric';
-
-let request = new XMLHttpRequest(); // créer un objet
-request.open('GET', url); //
-request.responseType = 'json';
-request.send();
-
-request.onload = function() {
-    if (request.readyState === XMLHttpRequest.DONE ) {
-      if (request.status === 200) {
-        let response = request.response;
-        let temp = response.main.temp;
-        console.log(temp);
-
-        document.querySelector('#temperature_label').textContent = temp;
-        document.querySelector('#ville').textContent = city;
-      }
-    } else {
-        alert('Un problème est survenu, revenez plus tard.');
-    }
-}
+let chosenCity = "Paris";
+getTemperature(chosenCity);
 
 let changeCity = document.querySelector('#changer');
 changeCity.addEventListener('click', () => {
-    city = window.prompt('Entrez le nom de votre ville');
+    chosenCity = prompt('Entrez le nom de votre ville');
+    getTemperature(chosenCity);
 });
+ 
+
+function getTemperature(city) {
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=227cf9ef448c3934d2b5f68324038f5d&units=metric';
+    
+    let request = new XMLHttpRequest(); // créer un objet
+
+    request.open('GET', url); //
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function() {
+        if (request.readyState === XMLHttpRequest.DONE ) {
+            if (request.status === 200) {
+                let response = request.response;
+                let temp = response.main.temp;
+                let city = response.name;
+                // console.log(temp);
+
+                document.querySelector('#temperature_label').textContent = temp;
+                document.querySelector('#ville').textContent = city;
+            }
+        } else {
+            alert('Un problème est survenu, revenez plus tard.');
+        }
+    }
+}
 
